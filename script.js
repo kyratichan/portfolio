3204078aa0c3e6b53472ec41fdfb9bd7930ebee7
Kyra Chan <kyratichan@gmail.com>
2025-05-16 20:39:44 -0400

    add images, social media links

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const currentPage = window.location.pathname.split('/').pop().split('.')[0] || 'index';
   console.log('Current page:', currentPage);  // Log currentPage
 
+  /* Loading nav bar */
   fetch('nav.html')
     .then(res => res.text())
     .then(html => {
@@ -32,4 +33,25 @@ document.addEventListener('DOMContentLoaded', () => {
     .catch(err => {
       console.error('Failed to fetch nav.html:', err);
     });
+
+  /* Fetching icon svgs */
+  const icons = [
+    { id: 'icon-email', path: 'assets/icons/email-icon.svg' },
+    { id: 'icon-linkedin', path: 'assets/icons/linkedin-icon.svg' },
+    { id: 'icon-instagram', path: 'assets/icons/ig-icon.svg' }
+  ];
+
+  icons.forEach(({ id, path }) => {
+    const container = document.getElementById(id);
+    if (container) {
+      fetch(path)
+        .then(res => res.text())
+        .then(svg => {
+          container.innerHTML = svg;
+        })
+        .catch(err => {
+          console.error(`Error loading ${path}:`, err);
+        });
+    }
+  });
 });
